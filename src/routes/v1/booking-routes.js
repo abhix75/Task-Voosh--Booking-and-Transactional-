f4ecfee88ac49c7f2890b28def9f68e68e089d6f
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-08-23 17:13:20 +0530

    updated

--- src/routes/v1/booking-routes.js
+++ src/routes/v1/booking-routes.js
@@ -3,10 +3,12 @@ const axios = require("axios");
 const { ServerConfig } = require("../../config");
 const { BookingController } = require("../../controllers");
 
-
 const router = express.Router();
 
-router.post("/", BookingController.createBooking);
+router.post(
+  "/",
+  BookingController.createBooking
+);
 router.post("/payments", BookingController.makePayment);
 
 module.exports = router;
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,7 +1,5 @@
 const axios = require("axios");
-/*
-Axios is a promise-based HTTP library that lets developers make requests to either their own or a third-party server to fetch data. It offers different ways of making requests such as GET , POST , PUT/PATCH , and DELETE .
-*/
+
 const { StatusCodes } = require("http-status-codes");
 const { Enums } = require("../utils/common");
 const { BOOKED, CANCELLED } = Enums.Booking_status;
@@ -12,10 +10,8 @@ const AppError = require("../utils/error/app-error");
 const bookingRepository = new BookingRepository();
 async function createBooking(data) {
 
-  // This is a Managed Transactions -> committing and rolling back the transaction should be done manually by the user (by calling the appropriate Sequelize methods).
 console.log('inside service create booking');
-  const transaction = await db.sequelize.transaction(); // Whenever I need to wrap a query within a transaction, I use the transaction object. I can pass the `transaction` object.
-  // Wrapping all of these in 1 transaction
+  const transaction = await db.sequelize.transaction(); 
 
   try {
     const menu = await axios.get(
@@ -28,22 +24,19 @@ console.log('inside service create booking');
     console.log(`TOTAL BILLING AMOUNT :`, totalBillingAmount);
 
     const bookingPayload = { ...data, totalCost: totalBillingAmount };
-    // When users send somethings we have currently userId, noOfSeats, flightId. In order to create a booking we need a totalCost as well so destructuring the object `data` using the spread operator `...data` and then adding one more key-value pair
     console.log("BookingPayLoad : ", bookingPayload);
     const booking = await bookingRepository.create(bookingPayload, transaction);
     console.log("after booking repo ")
-    // This is going to create a new booking for us and will be in an `INITIATED` state and the transaction will reserve the selected number of seats for the current booking for 5 mins for the end users to actually complete the payment, if not completed the payment on time then whatever no. of seats blocked by the transaction for the current booking should be released.
     await axios.patch(
       `${ServerConfig.MENU_SERVICE}/api/v1/menu/${data.menuId}/quantity`,
       {
-        quantity: data.quantity, // passing the data inside the req.body
+        quantity: data.quantity, 
       }
-    ); // Booking has been `INITIATED` so reserve noOfSeats in the actual flights or update the seats in the actual flights using patch()
-
-    await transaction.commit(); // If everything goes well do a commit
+    ); 
+    await transaction.commit(); 
     return booking;
   } catch (error) {
-    await transaction.rollback(); // If we get any error/anything fails above do a rollback
+    await transaction.rollback();
     console.log(error);
     throw error;
   }
@@ -57,6 +50,7 @@ async function makePayment(data) {
       data.bookingId,
       transaction
     );
+    console.log("Booking-Details",bookingDetails)
     if (bookingDetails.status == CANCELLED) {
       throw new AppError("The booking has expired", StatusCodes.BAD_REQUEST);
     }
@@ -149,7 +143,6 @@ async function cancelBooking(bookingId) {
     await transaction.rollback();
     if (error.statusCodes == StatusCodes.NOT_FOUND) {
         throw new AppError(
-          // error.message, //Overriding the error message thrown from the destroy(id) function inside the crud-repository file
           "For the request you made, there is no bookingId available to cancel!",
           error.statusCodes
         );
